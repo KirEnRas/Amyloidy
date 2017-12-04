a236f15ca1b8f111a3adb47a8fa18a9fab4f8eab
KirEnRas <34007481+KirEnRas@users.noreply.github.com>
2017-12-04 04:49:04 +0100

    Add files via upload

---
+++ Kolokwium5,2.py
@@ -0,0 +1,26 @@
+#Kolokwium5,2
+#Pawe³ Tomkowski
+a=raw_input("Podaj ci¹g cyfr:")
+l=list(a)
+for i in l:
+    if i=="0":
+         print "zero"
+    elif i=="1":
+        print "jeden"
+    elif i=="2":
+        print "dwa"
+    elif i=="3":
+        print "trzy"
+    elif i=="4":
+        print "cztery"
+    elif i=="5":
+        print "piêæ"
+    elif i=="6":
+        print "szeœæ"
+    elif i=="7":
+        print "siedem"
+    elif i=="8":
+        print "osiem"
+    elif i=="9":
+        print "dziewiêæ"
+    
